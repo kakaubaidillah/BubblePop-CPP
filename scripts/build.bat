efcbd19ec478b2abf4025a79d922fa69b4ee41d1
Kaka <k8782460@gmail.com>
2026-06-30 14:55:35 +0700

    Add gitignore, VS Code config, and build scripts

---
+++ scripts/build.bat
@@ -0,0 +1,16 @@
+@echo off
+
+cd /d "%~dp0.."
+
+"C:\mingw64\bin\g++.exe" ^
+src\main.cpp ^
+src\bubble.cpp ^
+src\ui.cpp ^
+src\game.cpp ^
+src\menu.cpp ^
+-I"C:\raylib\raylib\src" ^
+-L"C:\raylib\raylib\src" ^
+-lraylib -lgdi32 -lwinmm -lopengl32 ^
+-o BubblePop.exe
+
+pause
